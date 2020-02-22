a6daef66279e3f6c70ade278859417d66e1217e4
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-02-22 14:28:05 -0700

    Book API done

---
+++ BooksAPI/booksAPI.py
@@ -0,0 +1,10 @@
+import googlebooks
+api = googlebooks.Api()
+api_result = api.list('intitle:Harry Potter and the chamber of secrets')
+required_fields=['title','description','publisher','averageRating']
+result={}
+for field in required_fields:
+    result[field]=str(api_result['items'][0]['volumeInfo'][field])
+result['authors']=str(','.join(api_result['items'][0]['volumeInfo']['authors']))
+result['image']=api_result['items'][0]['volumeInfo']['imageLinks']['thumbnail']
+print(result)

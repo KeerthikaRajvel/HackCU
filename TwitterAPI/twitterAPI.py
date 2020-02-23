3fbfd35d72447fed86e993db73e283f65f10cd67
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-02-22 22:45:08 -0700

    Starting with Twitter API

--- TwitterAPI/twitterAPI.py
+++ TwitterAPI/twitterAPI.py
@@ -1,11 +1,13 @@
+import json
 import tweepy
-import config
-title='Harry Potter'
-tweet_urls = []
-auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
-auth.set_access_token(config.access_token, config.access_token_secret)
-api = tweepy.API(auth,wait_on_rate_limit=True)
-for tweet in tweepy.Cursor(api.search,q="#"+title,lang="en").items(20):
-  for url in tweet.entities['urls']:
-     if "twitter" in url['expanded_url']:
-        tweet_urls.append(str(tweet.id))
+from TwitterAPI import config
+def get_tweets(movie):
+    tweet_urls = []
+    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_token_secret)
+    api = tweepy.API(auth,wait_on_rate_limit=True)
+    for tweet in tweepy.Cursor(api.search,q="#"+movie,lang="en").items(20):
+      for url in tweet.entities['urls']:
+         if "twitter" in url['expanded_url']:
+            tweet_urls.append(str(tweet.id))
+    return json.dumps(tweet_urls)
--- start.py
+++ start.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from flask import jsonify
 from MongoDB import connection
-app=Flask(__name__)
+from TwitterAPI import twitterAPI
 
+app=Flask(__name__)
+movie="Sapiens"
 
 @app.route('/bookDetails')
-def bookDetails():
-   details=connection.search("Sapiens")
-   return render_template('bookDetails.html',book_details=details)
+def bookDetails(movie):
+   details=connection.search(movie)
+   tweets=twitterAPI.get_tweets(movie)
+   return render_template('bookDetails.html',book_details=details,tweets=tweets)
 
 if __name__ == '__main__':
    app.run(debug = True,port=3400)
